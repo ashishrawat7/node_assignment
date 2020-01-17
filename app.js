4c346bb443b2e7975799103c387bc32963f07592
ashishrawat7 <ashish895352@gmail.com>
2020-01-17 14:57:13 +0530

    second question of node study guide, 1 task to calculate the average of age, 2 task is to delete the document of users who's age is greater than 25.

--- app.js
+++ app.js
@@ -3,7 +3,18 @@ const http = require('http');
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
-const server = http.createServer(function (req, res) {
+const server = http.createServer(async function (req, res) {
+    var print = '';
+    function getAge(dateString) {
+        var today = new Date();
+        var birthDate = new Date(dateString);
+        var age = today.getFullYear() - birthDate.getFullYear();
+        var m = today.getMonth() - birthDate.getMonth();
+        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+            age--;
+        }
+        return age;
+    }
     res.writeHead(200, { 'Content-Type': 'application/json' });
     var user =
         [
@@ -12,52 +23,67 @@ const server = http.createServer(function (req, res) {
                 email: 'ashish@gmail.com',
                 last_name: 'rawat',
                 password: '1234',
-                profile: { dob: '', mobile_no: '' }
+                profile: { dob: '01/04/1994', mobile_no: '12345678' }
             },
             {
                 first_name: 'ramesh',
                 email: 'ramesh@gmail.com',
                 last_name: 'yadav',
                 password: '56789',
-                profile: { dob: '', mobile_no: '' }
+                profile: { dob: '10/06/1995', mobile_no: '999999999' }
             },
             {
                 first_name: 'nitesh',
                 email: 'nitesh@mail.com',
                 last_name: 'padney',
                 password: 'abcd',
-                profile: { dob: '', mobile_no: '' }
+                profile: { dob: '03/05/1999', mobile_no: '999999999999' }
             },
             {
                 first_name: 'sonu',
                 email: 'sonu@gmail.com',
                 last_name: 'kumar',
                 password: '12333',
-                profile: { dob: '', mobile_no: '' }
+                profile: { dob: '03/05/2000', mobile_no: '999999999999' }
             },
             {
                 first_name: 'deepak',
                 email: 'deepak@gmail.com',
                 last_name: 'maurya',
                 password: '1234',
-                profile: { dob: '', mobile_no: '' }
+                profile: { dob: '03/03/1990', mobile_no: '99999999999' }
             }
         ]
-
+    
     MongoClient.connect(url, async function (err, db) {
         if (err) throw err;
-        var dbo = db.db("assignmentFirst");
-        for await (element of user) {            
-            let data = await dbo.collection("users").insertOne(element)            
+        var dbo = await db.db("assignmentFirst");
+        for await (element of user) {        
+            let data = await dbo.collection("users").insertOne(element);
             await dbo.collection("usersProfile").insertOne({
                 _id: data.ops[0]._id,
-                dob: element.dob,
-                mobile_no: element.mobile_no
+                dob: element.profile.dob,
+                mobile_no: element.profile.mobile_no
             })
         }
+        await dbo.collection('users').find({}).toArray(function(err, docs) {
+            var dob = '';
+            var sum = 0;
+            docs.forEach(element => {
+                console.log(element);                
+                var age = '';
+                dob = element.profile.dob;
+                age = getAge(dob);                
+                if(age >= 25){                 
+                    dbo.collection('users').deleteOne({_id: element._id});
+                    dbo.collection('usersProfile').deleteOne({_id: element._id});                               
+                }
+                sum = sum + age;                
+            });            
+            console.log(sum/5);
+            res.end('got req. Record saved!<br>'+sum/5);            
+        });
     });
-    res.end(JSON.stringify(user));
-    res.end('got req. Record saved!');
 });
 
 server.listen(3000, '127.0.0.1');
